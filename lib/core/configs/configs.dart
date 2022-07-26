4ea8d97f5158c9e38360edefd65d2e05844cfbb0
roaa94 <roaa94khaddam@gmail.com>
2022-07-26 13:47:55 +0300

    chore: add TMDB api key to Configs

--- lib/core/configs/configs.dart
+++ lib/core/configs/configs.dart
@@ -2,4 +2,6 @@ class Configs {
   static const Duration maxCacheAge = Duration(hours: 1);
 
   static const String apiBaseUrl = 'https://api.themoviedb.org/3';
+
+  static const String tmdbAPIKey = String.fromEnvironment('TMDB_API_KEY', defaultValue: '');
 }
