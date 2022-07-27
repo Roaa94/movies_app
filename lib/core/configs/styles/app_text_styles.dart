a0a0b4dc0f3d6c267ccad790ebb2d4b0726ea4f3
roaa94 <roaa94khaddam@gmail.com>
2022-07-27 23:21:21 +0300

    chore: fix and change in app text styles

--- lib/core/configs/styles/app_text_styles.dart
+++ lib/core/configs/styles/app_text_styles.dart
@@ -4,8 +4,8 @@ class AppTextStyles {
   static const String fontFamily = 'Raleway';
 
   static const TextStyle bodyLg = TextStyle(
-    fontSize: 1,
-    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
   );
 
   static const TextStyle body = TextStyle(
@@ -15,12 +15,12 @@ class AppTextStyles {
 
   static const TextStyle bodySm = TextStyle(
     fontSize: 12,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w300,
   );
 
   static const TextStyle bodyXs = TextStyle(
     fontSize: 10,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w300,
   );
 
   static const TextStyle h1 = TextStyle(
@@ -35,6 +35,6 @@ class AppTextStyles {
 
   static const TextStyle h3 = TextStyle(
     fontSize: 20,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w400,
   );
 }
