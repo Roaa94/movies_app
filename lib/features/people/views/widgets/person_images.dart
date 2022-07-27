7c38bcaa92507e72c4a93e9d3f20e5c1aa9f67e0
roaa94 <roaa94khaddam@gmail.com>
2022-07-28 01:06:44 +0300

    fix: style fix in person details page

--- lib/features/people/views/widgets/person_images.dart
+++ lib/features/people/views/widgets/person_images.dart
@@ -18,67 +18,94 @@ class PersonImages extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      color: AppColors.primary,
-      padding: const EdgeInsets.only(
-        top: 20,
-        bottom: 20,
-      ),
-      // width: double.infinity,
-      child: ref.watch(personImagesProvider(personId)).when(
-        data: (List<PersonImage> images) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsetsDirectional.only(start: 40, end: 17),
-                child: Text(
-                  'Images',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                height: (images.length / 3).ceil() *
-                    ((MediaQuery.of(context).size.width - 10 * 4) / 3),
-                child: GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: images.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                  ),
-                  padding: const EdgeInsetsDirectional.only(start: 40, end: 17),
-                  itemBuilder: (context, index) => ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: images[index].thumbnail == null
-                        ? Container()
-                        : AppCachedNetworkImage(
-                            imageUrl: images[index].thumbnail!,
-                            fit: BoxFit.cover,
-                          ),
-                  ),
-                ),
-              ),
-            ],
-          );
-        },
-        error: (Object error, StackTrace? stackTrace) {
-          log('Error fetching popular people');
-          log(error.toString());
-          return const Icon(Icons.error);
-        },
-        loading: () {
-          return const Padding(
-            padding: EdgeInsetsDirectional.only(start: 40 - 17, end: 17),
-            child: GridShimmer(
-              minOpacity: 0.2,
-              maxOpacity: 0.3,
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 0,
+          height: 100,
+          left: 0,
+          right: 0,
+          child: Container(color: Theme.of(context).scaffoldBackgroundColor),
+        ),
+        Positioned(
+          top: 0,
+          height: 100,
+          left: 0,
+          right: 0,
+          child: Container(color: AppColors.secondary),
+        ),
+        ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(50),
+            bottomRight: Radius.circular(50),
+          ),
+          child: Container(
+            color: AppColors.primary,
+            padding: const EdgeInsets.only(
+              top: 20,
+              bottom: 20,
             ),
-          );
-        },
-      ),
+            // width: double.infinity,
+            child: ref.watch(personImagesProvider(personId)).when(
+              data: (List<PersonImage> images) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsetsDirectional.only(start: 40, end: 17),
+                      child: Text(
+                        'Images',
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      height: (images.length / 3).ceil() *
+                          ((MediaQuery.of(context).size.width - 10 * 4) / 3),
+                      child: GridView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: images.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                        ),
+                        padding: const EdgeInsetsDirectional.only(
+                            start: 40, end: 17),
+                        itemBuilder: (context, index) => ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: images[index].thumbnail == null
+                              ? Container()
+                              : AppCachedNetworkImage(
+                                  imageUrl: images[index].thumbnail!,
+                                  fit: BoxFit.cover,
+                                ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+              error: (Object error, StackTrace? stackTrace) {
+                log('Error fetching popular people');
+                log(error.toString());
+                return const Icon(Icons.error);
+              },
+              loading: () {
+                return const Padding(
+                  padding: EdgeInsetsDirectional.only(start: 40 - 17, end: 17),
+                  child: GridShimmer(
+                    minOpacity: 0.2,
+                    maxOpacity: 0.3,
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
