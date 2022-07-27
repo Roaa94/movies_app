910afea01650c3808005a7cf36b8416031dea636
roaa94 <roaa94khaddam@gmail.com>
2022-07-27 16:00:09 +0300

    refactor: core reformat

--- lib/features/people/providers/popular_people_count.dart
+++ lib/features/people/providers/popular_people_count.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movies_app/core/models/paginated_response.dart';
+import 'package:movies_app/features/people/models/person.dart';
 import 'package:movies_app/features/people/providers/popular_people_provider.dart';
 
 final popularPeopleCountProvider = Provider<AsyncValue<int>>((ref) {
-  return ref
-      .watch(paginatedPopularPeopleProvider(0))
-      .whenData((pageData) => pageData.totalResults);
+  return ref.watch(paginatedPopularPeopleProvider(0)).whenData(
+        (PaginatedResponse<Person> pageData) => pageData.totalResults,
+      );
 });
