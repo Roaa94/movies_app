c58f18bed26e6167ab5055ff2e05ba2d28f85b34
roaa94 <roaa94khaddam@gmail.com>
2022-07-27 14:57:32 +0300

    chore: initialize storage service and wrap app with ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:movies_app/core/services/storage/hive_storage_service.dart';
+import 'package:movies_app/core/services/storage/storage_service.dart';
+import 'package:movies_app/core/services/storage/storage_service_provider.dart';
 import 'package:movies_app/movies_app.dart';
 
 void main() {
-  runApp(const MoviesApp());
+  runZonedGuarded<Future<void>>(() async {
+    // Hive-specific initialization
+    await Hive.initFlutter();
+    final StorageService initializedStorageService = HiveStorageService();
+    await initializedStorageService.init();
+
+    runApp(
+      ProviderScope(
+        overrides: [
+          storageServiceProvider.overrideWithValue(initializedStorageService),
+        ],
+        child: const MoviesApp(),
+      ),
+    );
+  }, (e, _) => throw e);
 }
