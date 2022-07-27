24e40a8c8dd68781706750d489b84d6f8e8a7208
roaa94 <roaa94khaddam@gmail.com>
2022-07-27 23:52:23 +0300

    feat: add fade-in animation to app bar leading

--- lib/core/widgets/app_bar_leading.dart
+++ lib/core/widgets/app_bar_leading.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/core/configs/styles/app_colors.dart';
 
-class AppBarLeading extends StatelessWidget {
+class AppBarLeading extends StatefulWidget {
   const AppBarLeading({Key? key}) : super(key: key);
 
   @override
+  State<AppBarLeading> createState() => _AppBarLeadingState();
+}
+
+class _AppBarLeadingState extends State<AppBarLeading>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animationController;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    Future.delayed(const Duration(milliseconds: 200)).then((_) {
+      animationController.forward();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ClipOval(
-        child: Material(
-          color: AppColors.primary,
-          child: InkWell(
-            onTap: () => Navigator.of(context).pop(),
-            child: const Padding(
-              padding: EdgeInsets.all(12),
-              child: Icon(Icons.arrow_back),
+    return FadeTransition(
+      opacity: animationController,
+      child: Center(
+        child: ClipOval(
+          child: Material(
+            color: AppColors.primary,
+            child: InkWell(
+              onTap: () => Navigator.of(context).pop(),
+              child: const Padding(
+                padding: EdgeInsets.all(12),
+                child: Icon(Icons.arrow_back),
+              ),
             ),
           ),
         ),
