01974d495b86167d816ec52fe9eaf7d6498eeb4d
roaa94 <roaa94khaddam@gmail.com>
2022-08-07 15:04:37 +0300

    chore: test case: currentPopularPersonProvider UnimplementedError

---
+++ test/features/people/providers/current_popular_person_provider_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:movies_app/features/people/providers/current_popular_person_provider.dart';
+
+void main() {
+  test('current popular person provider throws UnimplementedError initially',
+      () {
+    final providerContainer = ProviderContainer();
+
+    addTearDown(providerContainer.dispose);
+
+    expect(
+      () => providerContainer.read(currentPopularPersonProvider),
+      throwsA(isA<ProviderException>()),
+    );
+
+    try {
+      providerContainer.read(currentPopularPersonProvider);
+    } on ProviderException catch (e) {
+      expect(e.exception, isA<UnimplementedError>());
+    }
+  });
+}
