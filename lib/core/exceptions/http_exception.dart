169f70f547e407be70888abdca6f9db259cbed0a
roaa94 <roaa94khaddam@gmail.com>
2022-09-14 22:38:47 +0300

    chore: resolve very good analysis warnings in core folder

--- lib/core/exceptions/http_exception.dart
+++ lib/core/exceptions/http_exception.dart
@@ -1,11 +1,18 @@
+/// Custom exception used with Http requests
 class HttpException implements Exception {
-  final String? title;
-  final String? message;
-  final int? statusCode;
-
+  /// Creates a new instance of [HttpException]
   HttpException({
     this.title,
     this.message,
     this.statusCode,
   });
+
+  /// Exception title
+  final String? title;
+
+  /// Exception message
+  final String? message;
+
+  /// Exception http response code
+  final int? statusCode;
 }
