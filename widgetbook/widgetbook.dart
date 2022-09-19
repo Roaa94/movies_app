c28e0e4896fd82fc33ee00da24f036ce002af6fe
roaa94 <roaa94khaddam@gmail.com>
2022-09-19 11:07:46 +0300

    feat: adds PersonDetailsPage to widgetbook

--- widgetbook/widgetbook.dart
+++ widgetbook/widgetbook.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movies_app/features/people/views/pages/person_details_page.dart';
 import 'package:widgetbook/widgetbook.dart';
+
+import '../test/test-utils/dummy-data/dummy_people.dart';
 
 class WidgetbookHotReload extends StatelessWidget {
   const WidgetbookHotReload({Key? key}) : super(key: key);
@@ -10,6 +14,33 @@ class WidgetbookHotReload extends StatelessWidget {
       categories: [
         WidgetbookCategory(
           name: 'material',
+          folders: [
+            WidgetbookFolder(
+              name: 'Pages',
+              widgets: [
+                WidgetbookComponent(
+                  name: 'Person Details',
+                  useCases: [
+                    WidgetbookUseCase(
+                      name: '',
+                      builder: (context) {
+                        return ProviderScope(
+                          child: MaterialApp(
+                            home: PersonDetailsPage(
+                              personId: DummyPeople.person1.id!,
+                              personName: DummyPeople.person1.name,
+                              personAvatar: null,
+                              personGender: DummyPeople.person1.gender,
+                            ),
+                          ),
+                        );
+                      },
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
           widgets: [
             WidgetbookComponent(
               name: 'FAB',
